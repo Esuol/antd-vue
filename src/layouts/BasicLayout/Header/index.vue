221e88936c9d5fe169d4cc58bda1df2203297664
徐加辉 <qjerryxjh@163.com>
2019-04-04 18:50:53 +0800

    international

--- src/layouts/BasicLayout/Header/index.vue
+++ src/layouts/BasicLayout/Header/index.vue
@@ -20,6 +20,17 @@
             </a-menu-item>
           </a-menu>
         </a-dropdown>
+        <a-select
+          :default-value="defalutValue"
+          style="width: 80px;marginRight:20px"
+          @change="handleChange">
+          <a-select-option
+            v-for="(item,index) in languages"
+            :key="index"
+            :value="item.value">
+            {{ item.name }}
+          </a-select-option>
+        </a-select>
       </div>
     </a-layout-header>
     <breadcrumb />
@@ -39,14 +50,29 @@ export default {
       type: Boolean
     }
   },
+  data () {
+    return {
+      defalutValue: '中文'
+    }
+  },
   computed: {
     ...mapState({
       userInfo: state => state.user.info
-    })
+    }),
+    languages () {
+      return [
+        { name: this.$t('common.chinese'), value: 'cn' },
+        { name: this.$t('common.english'), value: 'en' }
+      ]
+    }
   },
   methods: {
     logout () {
       this.$store.dispatch('user/logout')
+    },
+    handleChange (item) {
+      const Transform = new CustomEvent('selectLanguage', { 'detail': item })
+      window.dispatchEvent(Transform)
     }
   }
 }
